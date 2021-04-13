4ace99d7efde768e8d54d510bd6ff3b3115b7d24
Yujun Zhang <ericzh718@gmail.com>
2021-04-14 06:02:30 +1200

    feat(loginfo/usercontext): add usercontext and logInfo files

--- src/account/Login.js
+++ src/account/Login.js
@@ -1,17 +1,25 @@
 import react from "react";
 
+import LoginInfo from "./LoginInfo";
 import LoginButton from "./LoginButton.js";
 import SignupButton from "./SignupButton.js";
 
+import * as dummyLogin from "./dummyJSON/dummyLogin.json";
+
+const UserContext = react.createContext({dummyLogin});
+
 const Login = () => {
-    return <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        }}>
-        <span><LoginButton /></span>
-        <span><SignupButton /></span>
-    </div>
+    return <UserContext.Provider value = {{user: dummyLogin}}>
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexWrap: 'wrap',
+                }}>
+                <span><LoginInfo /></span>
+                <span><LoginButton /></span>
+                <span><SignupButton /></span>
+            </div>
+    </UserContext.Provider>
 }
 
 export default Login;
---
+++ src/account/LoginInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function LoginInfo() {
+    return <p>
+        nothing yet
+    </p>
+}
