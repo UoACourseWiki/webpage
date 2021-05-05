1bbd3da6f8e83f0dab3c5d9689b857549d16beb8
RukeW <rwei890@aucklanduni.ac.nz>
2021-05-05 13:56:50 +1200

    feat(subjectView):onclick

--- src/subject/subjectView.js
+++ src/subject/subjectView.js
@@ -1,16 +1,22 @@
 
 import styles from './subjectView.module.css';
 
-
+ function handleClick(){
+    
+        console.log('Click happened');
+      
+}
 export default  function SubjectView({courses, error}){  
     if (error !== undefined) {
-      return <p>{error.status}:{error.data.title} </p>
+      return <div><h1 className={styles.container}>{courses } {error.data.title}</h1> <button className={styles.button}>BACK</button></div>
     } 
-  
+ else  
       return (<div>
         <p className={styles.p}>All courses</p>
         {courses.map((c,i)=>(
-      <button className={styles.subject} key={i} >
+
+         
+      <button onClick={()=> handleClick()} className={styles.subject} key={i} >
         <div className={styles.title}>{c.subject} {  }
         {c.catalogNbr} </div>
         <div>{c.title}</div>
