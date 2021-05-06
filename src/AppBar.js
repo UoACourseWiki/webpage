dc541badfe042d603bbcfc9ee29856b05919c173
Joenan <joenan.woo@gmail.com>
2021-05-07 01:26:23 +1200

    feat(topbar): add account icon button to top bar

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,16 +1,22 @@
-import React from "react";
-import { AppBar, Toolbar, IconButton } from "@material-ui/core";
+import { useState } from "react";
+import { AppBar, Toolbar, IconButton, Menu, MenuItem } from "@material-ui/core";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 
 import SearchPage from "./search/searchPage";
 import styles from "./AppBar.module.css";
+import { profilePath } from "./utils/URLPath";
+import { useHistory } from "react-router";
 
 export default function PrimarySearchAppBar() {
-  const handleProfileMenuOpen = (event) => {};
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleProfileMenuOpen = () => {
+    setOpenMenu(true);
+  };
 
   return (
     <div className={styles.grow}>
-      <AppBar position="static">
+      <AppBar color="default" position="static">
         <Toolbar>
           <div className={styles.search}>
             <SearchPage />
@@ -29,6 +35,32 @@ export default function PrimarySearchAppBar() {
           </div>
         </Toolbar>
       </AppBar>
+      <AccountMenu
+        isMenuOpen={openMenu}
+        closeMenu={() => {
+          setOpenMenu(false);
+        }}
+      />
     </div>
   );
 }
+
+const AccountMenu = ({ isMenuOpen, closeMenu }) => {
+  const history = useHistory();
+
+  function handleClickProfile() {
+    closeMenu();
+    history.push(profilePath);
+  }
+
+  return (
+    <Menu
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={closeMenu}
+    >
+      <MenuItem onClick={handleClickProfile}>Profile</MenuItem>
+    </Menu>
+  );
+};
