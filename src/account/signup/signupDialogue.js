ee2f93f3358fdf0e828af3acc29ac4f87a8cd815
Joenan <joenan.woo@gmail.com>
2021-05-06 13:04:50 +1200

    feat(3.3 signup): add email address validation to InputText

--- src/account/signup/signupDialogue.js
+++ src/account/signup/signupDialogue.js
@@ -1,21 +1,27 @@
-import { Button } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
+import { useState } from "react";
+import { validEmail } from "./validator";
 
 const SignupDialogue = ({ updateInfo, handleSubmit, error }) => {
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
   return (
     <div>
       <h2>Sign Up</h2>
+
       <label>Your Nick Name</label>
       <input
         type="text"
         onChange={(e) => updateInfo({ nm: e.target.value })}
       ></input>
       <br />
-      <label>Email</label>
-      <input
-        type="text"
-        onChange={(e) => updateInfo({ em: e.target.value })}
-      ></input>
+
+      <EmailInputText
+        updateInfo={updateInfo}
+        updateEnableSubmit={setEnableSubmit}
+      />
       <br />
+
       <label>Password</label>
       <input
         type="text"
@@ -29,8 +35,45 @@ const SignupDialogue = ({ updateInfo, handleSubmit, error }) => {
       ></input>
       <br />
       <p style={{ color: "red" }}>{error}</p>
-      <Button onClick={handleSubmit}>Sign Up</Button>
+      <Button disabled={!enableSubmit} onClick={handleSubmit}>
+        Sign Up
+      </Button>
     </div>
+  );
+};
+
+const EmailInputText = ({ updateInfo, updateEnableSubmit }) => {
+  const [email, setEmail] = useState("");
+  const [emailValid, setEmailValid] = useState(true);
+  const [emailText, setEmailText] = useState("");
+
+  const handleEmailBlur = () => {
+    var text = "";
+    var valid = validEmail(email);
+    if (valid) {
+      updateInfo({ em: email });
+    } else {
+      text =
+        email.length === 0 ? "this field is required" : "email is not valid";
+    }
+
+    setEmailValid(valid);
+    setEmailText(text);
+    updateEnableSubmit(valid);
+  };
+
+  return (
+    <TextField
+      variant="outlined"
+      label="Email"
+      value={email}
+      error={!emailValid}
+      onBlur={handleEmailBlur}
+      onChange={(e) => {
+        setEmail(e.target.value);
+      }}
+      helperText={emailText}
+    />
   );
 };
 
